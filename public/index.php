afb2bdf3c9134eada4a0e31dc4ed3573402f5771
Niklas Maier <n.maier@syzygy.de>
2016-02-08 12:58:07 +0100

    enabled CORS

--- public/index.php
+++ public/index.php
@@ -3,6 +3,26 @@
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
  */
+
+// Allow from any origin
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    echo "options";
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+}
+
 chdir(dirname(__DIR__));
 
 // Decline static file requests back to the PHP built-in webserver
