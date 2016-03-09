68e070f73f98269d1936daf42b15b37e5524ac90
Jakob <Jakob@Jakob-PC>
2016-03-09 17:13:11 +0100

    Added activate key for inactiveusercontroller

--- module/ShipsUnburned/src/ShipsUnburned/Controller/InactiveUserController.php
+++ module/ShipsUnburned/src/ShipsUnburned/Controller/InactiveUserController.php
@@ -25,7 +25,7 @@ class InactiveUserController extends AbstractRestfulController
         if ($request->isPost())
         {
             $request = json_decode(file_get_contents('php://input'), true);
-            $result = $this->userTable->setUsersActive($request);
+            $result = $this->userTable->setUsersActive($request["activate"]);
         }
         return new JsonModel($result);
     }
