7e3095385c5129fe005823bf9b46132349e08dc3
Jakob <Jakob@Jakob-PC>
2016-03-11 17:46:06 +0100

    added 2 dimensional array and a function to fill it with given array

--- module/ShipsUnburned/src/ShipsUnburned/Model/Entity/Game.php
+++ module/ShipsUnburned/src/ShipsUnburned/Model/Entity/Game.php
@@ -12,8 +12,36 @@ class Game implements GameInterface
         return $this->gamefield;
     }
     
-    public function setGameField(Array $gamefield)
+    public function setGameField()
     {
-        $this->gamefield = $gamefield;
+        $a = array(); 
+        $b = array(); 
+        $b = array_pad($b,12,0); 
+        $a = array_pad($a,12,$b); 
+        
+        $this->gamefield = $a;
+    }
+    /**
+     * Insert Ships into the 2 dimensional array
+     * @param array $array
+     */
+    public function insertShipsIntoGameField(array $array)
+    {
+        $gamefield = $this->gamefield;
+        
+        //Go through the gamefield
+        for ($i = 0; $i < count($gamefield); $i++)
+        {
+            //When array[$i] has values go into next for loop
+            if ($array[$i] != null)
+            {
+                //Go through the value/s
+                for ($j = 0; $k < count($array[$i]); $j++)
+                {
+                    //Set Gamefield = 1 where gamefield[$i] and the value of $array[$i][$j]
+                    $gamefield[$i][$array[$i][$j]] = 1;
+                }
+            }
+        }
     }
 }
